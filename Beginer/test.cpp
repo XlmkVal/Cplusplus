3728e11d26cc00bb2ed63863c2e104b371920bb6
Xlmk <vh.dakov@gmail.com>
2022-05-31 10:34:59 +0300

    cleaning

--- Beginer/test.cpp
+++ Beginer/test.cpp
@@ -1,9 +1,15 @@
+
 #include<iostream>
 
 using namespace std;
 
+enum class Colors {black, blue, green, cyan, red, purple, yellow, white};
+
 int main()
 {
+    Colors dogs;
+    dogs = Colors::black;
+    cout << dogs << endl;
     char c = 'n';
     cout << int{c} << endl;
     return 0;
